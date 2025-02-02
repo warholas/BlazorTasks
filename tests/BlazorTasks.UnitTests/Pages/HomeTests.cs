5e44adb0775c0fdf293ac8a3c0061dc40b659cdd
Nicholas Warren <nwarren11@gmail.com>
2025-02-02 07:12:53 -0600

    Adding initial blazor and xunit projects

---
+++ tests/BlazorTasks.UnitTests/Pages/HomeTests.cs
@@ -0,0 +1,19 @@
+﻿using Bunit;
+using Xunit;
+using BlazorTasks.Components.Pages;
+namespace BlazorTasks.UnitTests.Pages;
+public class HomeTests : TestContext
+{
+    [Fact]
+    public void Home_ShouldRenderCorrectly_WhenInDefaultState()
+    {
+        // Arrange & Act
+        var cut = RenderComponent<Home>();
+
+        // Assert
+        cut.MarkupMatches(@"
+            <h1>Hello, world!</h1>
+            Welcome to your new app.
+        ");
+    }
+}
