eb34dba95226377b55e56b660eec64eb8bb1c469
Nicholas Warren <nwarren11@gmail.com>
2025-02-04 16:49:14 -0600

    Adding task app components

---
+++ src/BlazorTasks/Models/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorTasks.Models
+{
+    public class TaskItem
+    {
+        public string? Name { get; set; } = string.Empty;
+        public bool IsEditMode { get; set; } = false;
+        public bool IsCompleted { get; set; } = false;
+        public string ElementClass { get; set; } = string.Empty;
+        public ElementReference Reference { get; set; } = new ElementReference();
+    }
+}
